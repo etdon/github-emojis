472f394ff105c300398daea28abd6d69a20a78df
etdon <169199686+etdon@users.noreply.github.com>
2024-12-30 19:47:01 +0100

    Update update.py

--- update.py
+++ update.py
@@ -2,7 +2,6 @@ import requests
 
 emoji_data = requests.get("https://api.github.com/emojis", headers={
     "Accept": "application/vnd.github+json",
-    "Authorization": "Bearer ${{GITHUB_TOKEN}}",
     "X-GitHub-Api-Version": "2022-11-28"
   }
 )
