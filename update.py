1d2f3663d9c56faf02c3a3bc4ccc64c13da31370
etdon <169199686+etdon@users.noreply.github.com>
2024-12-30 20:19:02 +0100

    Update update.py

--- update.py
+++ update.py
@@ -6,6 +6,19 @@ emoji_data = requests.get("https://api.github.com/emojis", headers={
   }
 )
 
+formatted_data = [ "|||||", "|---|---|---|---|" ]
+line_components = []
+for key, value in emoji_data.json().items():
+    line_components.append(f":{key}: `{key}`|")
+    if (len(line_components) == 4):
+        formatted_data.append(f"|{''.join(line_components)}")
+        line_components.clear()
+if (len(line_components) > 0):
+    for i in range(4 - len(line_components)):
+        line_components.append("|")
+    formatted_data.append(f"|{''.join(line_components)}")
+    line_components.clear()
+
 readme_file = open("README.md", "w+")
-readme_file.write(emoji_data.text)
+readme_file.writelines(formatted_data)
 readme_file.close()
